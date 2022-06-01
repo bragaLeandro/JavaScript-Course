dd37d9d112f724a00d18844f0e3a1e17e375b945
bragaLeandro <rm94513@fiap.com.br>
2022-06-01 07:15:45 -0300

    Constants with const

---
+++ aula7/index.js
@@ -0,0 +1,13 @@
+// Não podemos criar constantes com palavras reservadas
+// constantes precisam ter nomes significativos
+// Não pode começar o nome de uma variável com número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE CONST.
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultDuplicado = resultado * 2;
+
