46fe5818c0d6fe04a972bb2d3fa616f949334682
bragaLeandro <rm94513@fiap.com.br>
2022-06-02 03:46:01 -0300

    Primitive data types

---
+++ aula10/index.js
@@ -0,0 +1,9 @@
+// String, number, undefined, null, boolean, symbol
+const nome = 'Leandro'; //string
+const nome1 = "Leandro"; //string
+const nome2 = `Leandro`; //string
+const num1 = 10; //number
+const num2 = 10.52; //number
+let nomeAluno; //undefined -> não aponta para local nenhum na memória
+const sobrenomeAluno = null; //Nulo -> não aponta para local nenhum na memória
+const aprovado = false; //Bolean = true, false (lógico)
