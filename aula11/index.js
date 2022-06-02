e941c6726014cf75d196d8d52e714ae0fbfdb999
bragaLeandro <rm94513@fiap.com.br>
2022-06-02 04:24:25 -0300

    Operators

---
+++ aula11/index.js
@@ -0,0 +1,18 @@
+/**
+ * Aritméticos
+ * + Adição / Concatenação
+ * - Subtração
+ * * Multiplicação
+ * / Divisão
+ * ** Potenciação
+ * % Resto da divisão
+ * ++ Incremento
+ * -- Decrement0
+ * 
+ * Operadores de atribuição
+ * +=
+ * -=
+ */
+
+let contador = 1;
+contador++
