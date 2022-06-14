0b978e2877496d405a19ed7b38d24d573c33f985
bragaLeandro <rm94513@fiap.com.br>
2022-06-14 12:35:40 -0300

    String methods

---
+++ aula14/script.js
@@ -0,0 +1,14 @@
+let umaString = 'o rato roeu a roupa do rei de roma';
+/* 
+console.log(umaString.charAt(4));
+console.log(umaString.indexOf('t', 1));
+console.log(umaString.lastIndexOf('t', 5));
+console.log(umaString.match(/[a-z]/g));
+console.log(umaString.replace(/r/g, 'G'));
+console.log(umaString.length);
+console.log(umaString.slice(-4));
+console.log(`${umaString} em um lindo dia.`);
+console.log(umaString.split(' ', 2));
+*/
+console.log(umaString.toUpperCase());
+console.log(umaString.toLowerCase());
